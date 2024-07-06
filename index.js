87e906fce05415bad8f65eeb54b00415b980cc49
ArtyomHloba <hlobartyom@gmail.com>
2024-07-06 19:30:56 +0300

    Add props 2

--- index.js
+++ index.js
@@ -6,40 +6,14 @@ const root = ReactDOM.createRoot(roodEl);
 root.render(<App/>);
 
 function App() {
-    const news = [
-      {
-        title: "Some news1",
-        body: "Something hapenned1",
-        isGood: false,
-      },
-      {
-        title: "Some news2",
-        body: "Something hapenned2",
-        isGood: true,
-      },
-    ];
+    const isGreeting = true;
+    const userName = 'Ivo'
     return (
-      <>
-        <News newsItem={news[0]} />
-        <News newsItem={news[1]} />
-      </>
+      <Greeting isGreeting={isGreeting} userName={userName}/>
     );
   }
-  // News(news[0])
-  // News(news[1])
-  // <News newsItem={news[0]} /> => News({newsItem:news[0]})
-  
-  function News(props) {
-    const news = props.newsItem;
-    const newsStyle = {
-      border: `3px solid ${news.isGood ? "green" : "red"}`,
-    };
-  
-    return (
-      <article style={newsStyle}>
-        <h2>{news.title}</h2>
-        <p>{news.body}</p>
-      </article>
-    );
+  function Greeting(props){
+    const {isGreeting, userName} = props
+    return <div>{isGreeting ?'Hello':'Goodbye'}, {userName}</div>
   }
   
